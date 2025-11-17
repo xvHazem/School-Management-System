dfae6b1be1ca694108e46f61f66f380ca59ca860
xvhazem <xvhazem@gmail.com>
2025-11-17 21:19:45 +0300

    new

--- src/main/java/schoolmanagement/HigherSecondaryStudent.java
+++
@@ -1,7 +0,0 @@
-package schoolmanagement;
-
-public class HigherSecondaryStudent extends Student {
-    public HigherSecondaryStudent(String studentId, String studentName, String classId, String section, String busId) {
-        super(studentId, studentName, classId, section, busId);
-    }
-}
