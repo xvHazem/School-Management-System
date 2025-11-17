558002c604ee57844589e02b4990c6b5cc2cef86
xvhazem <xvhazem@gmail.com>
2025-11-17 21:40:14 +0300

    Revert files to commit b813394e68122285f62de91d6afd3cc311e43cf4

---
+++ src/main/java/com/mycompany/schoolsystem/SchoolSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.schoolsystem;
+
+/**
+ *
+ * @author ixhaz
+ */
+public class SchoolSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
